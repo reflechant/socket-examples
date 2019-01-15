2a9904a30212afde01dc61ebb6921a8bb349f573
R.G <reflechant@users.noreply.github.com>
2019-01-16 02:15:22 +0300

    fixed 127.0.0.1 being returned as address in Linux

--- udp_send.py
+++ udp_send.py
@@ -1,15 +1,19 @@
 """
-This program sends one broadcast UDP packet over local network
+This program sends one UDP packet over local network
 """
 import socket
 import ipaddress
+peer_addr = '192.168.1.10'
 port = 22000
-netmask = '255.255.255.0'
 
-my_addr = socket.gethostbyname(socket.gethostname())
 
-broadcast_addr = '.'.join(my_addr.split('.')[:-1]+['255'])
+def get_host():
+    with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as s:
+        s.connect(('10.255.255.255', 80))
+        return s.getsockname()[0]
+
 
 with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as sock:
-    sock.bind((my_addr, port))
-    sock.sendto("Hello, world !".encode(), 0, (broadcast_addr, port))
+    sock.bind((get_host(), port))
+    # broadcast_addr = '.'.join(my_addr.split('.')[:-1] + ['255'])
+    sock.sendto("Hello, world !".encode(), 0, (peer_addr, port))
